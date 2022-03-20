eec19ebe974dd996cc3a8f4cc805337771dfac86
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2022-03-20 18:02:46 +0530

    Level 3: hashing and salting

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt')
+
 
 const app = express();
 
@@ -21,60 +21,60 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = process.env.SCERET_KEY;
-userSchema.plugin(encrypt, { secret: secret , encryptedFields:["password"]});
+
 
 const User = mongoose.model('User', userSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  const newUser =  new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+app.post("/register", function (req, res) {
+
+  bcrypt.hash(req.body.password, 10, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
+  User.findOne({email:username},(err,foundUser)=>{
+    if(!err)
+    {
+      bcrypt.compare(password, foundUser.password, function(error, result) {
+        if(result)res.render("secrets")
+        else res.redirect("/login")
+    });
     }
-  });
-});
+    else console.log(err)
+  })
+
+})
 
 
 
 
 
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
-});
+app.listen("3000",()=>{
+  console.log("server running")
+})
